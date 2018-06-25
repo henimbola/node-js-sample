38c2de89e14f17b7aa015303bb72f1117539657c
Henimbola <henimbola@gmail.com>
2018-06-25 14:45:03 +0200

    First save

---
+++ server.js
@@ -0,0 +1,28 @@
+
+//Importations des besoins
+let http = require('http');
+let fs = require('fs');
+let url = require('url');
+
+//Creation du serveur
+let server = http.createServer((req, res) => {
+
+  //Ouverture du fichier à lire
+  fs.readFile('indeax.html',(err, data) => {
+
+    if (err) throw err;
+
+    res.writeHead(200, {
+      "Content-Type" : "text/html ; charset=utf-8"
+    });
+
+    res.end(data);
+    console.log('Requête');
+
+  })
+
+
+});
+
+
+server.listen(8080);
