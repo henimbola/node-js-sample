ca1fc112d1a3ff470f577fa186314f56588c78b0
henimbola <henimbola@gmail.com>
2020-02-16 14:26:14 +0300

    Return from zero

--- server.js
+++ server.js
@@ -1,28 +1,11 @@
-
-//Importations des besoins
 let http = require('http');
 let fs = require('fs');
 let url = require('url');
 
-//Creation du serveur
 let server = http.createServer((req, res) => {
 
-  //Ouverture du fichier à lire
-  fs.readFile('index.html',(err, data) => {
-
-    if (err) throw err;
-
-    res.writeHead(200, {
-      "Content-Type" : "text/html ; charset=utf-8"
-    });
-
-    res.end(data);
-    console.log('Requête');
-
-  })
-
-
+  res.writeHead(200);
+  res.end('Hi Everyone');
 });
-
 
 server.listen(8080);
