0789316bfe6dbdc5d96aeee682fe2cdf38e5c544
Henimbola ANDRIANATOANDRO <henimbola@gmail.com>
2022-06-26 22:50:00 +0300

    refactor(express): update endpoint

--- server.ts
+++ server.ts
@@ -1,24 +1,31 @@
-var express = require('express');
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
-var app = express();
+const app = express()
+
+app.use(cors())
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
   res.setHeader('Content-Type', 'text/plain');
   res.send('Vous êtes à l accueil');
+});
+
+app.get('/name/:name', (req, res) => {
+  res.render('templates/display-name.ejs', { name: req.params.name });
 })
 
-  .get('/name/:name', (req, res) => {
-    res.render('templates/display-name.ejs', { name: req.params.name });
-  })
+app.get('/api/info/:name', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send({ info: `test ${req.params.name}` });
+})
 
-  .get('/api/info/:name', (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.send({ info: `test ${req.params.name}` });
-  })
-
-  .use((req, res, next) => {
-    res.setHeader('Content-Type', 'text/plain');
-    res.status(404).send('404 !! Page not found.');
-  });
+// .use((req, res, next) => {
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.status(404).send('404 !! Page not found.');
+// });
 
 app.listen(8080);
