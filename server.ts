3fde8b7d10eaa476ee0f61177fa823236a3ddb7a
Henimbola ANDRIANATOANDRO <henimbola@gmail.com>
2022-06-26 23:40:37 +0300

    core(pug): add pug.js as templating engine

--- server.ts
+++ server.ts
@@ -4,11 +4,16 @@ const cors = require('cors')
 const multer = require('multer')
 
 const app = express()
+const upload = multer({ dest: 'tmp/' })
+const pug = require('pug')
 
 app.use(cors())
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+app.set('views', './views/templates')
+app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
   res.setHeader('Content-Type', 'text/plain');
@@ -24,15 +29,12 @@ app.get('/api/info/:name', (req, res) => {
   res.send({ info: `test ${req.params.name}` });
 })
 
-// .use((req, res, next) => {
-//   res.setHeader('Content-Type', 'text/plain');
-//   res.status(404).send('404 !! Page not found.');
-// });
-
-const upload = multer({ dest: 'tmp/' })
-
 app.post("/upload_file", upload.single('file'), (req, res, next) => {
   res.send({ status: "success" })
 })
+
+app.get("/pug_view", (req, res) => res.render('file-upload', {
+  youAreUsingPug: true
+}));
 
 app.listen(8080);
